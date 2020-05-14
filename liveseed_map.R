794370ee2c20f474ab019d4a32b627d67f216e7b
Mariateresa Lazzaro <64500069+mariateresalazzaro@users.noreply.github.com>
2020-05-14 14:23:12 +0200

    map plant breeding v1.9

--- liveseed_map.R
+++ liveseed_map.R
@@ -21,35 +21,78 @@ library(dplyr)
 ## PART 1 - IN THIS PART THE CODE READS THE FILES AND ATTRIBUTES COLORS AND ICONS TO ELEMENTS
 
 ## Read the shapefile
-countries <- readOGR('Europe/Europe.shp')
-countries$NUMBER<-c(0,0,5,2,1,0,0,2,1,2,6,15,0,1,0,1,1,0,6,0,1,0,0,0,1,0,0,0,8,0,1,3,0,0,0,0,13,0,9,2,0,0,0,0,0,0,0,0,0,2,0,1,0,0)
+#countries <- readOGR('Europe/Europe.shp')
+#countries$NUMBER<-c(0,0,5,2,1,0,0,2,1,2,6,15,0,1,0,1,1,0,6,0,1,0,0,0,1,0,0,0,8,0,1,3,0,0,0,0,13,0,9,2,0,0,0,0,0,0,0,0,0,2,0,1,0,0)
 ## Create the palette of colors for the shapefiles
-palette_countries <- colorNumeric(palette = "YlOrRd", domain = countries$CNTR_BN_ID)
+#palette_countries <- colorNumeric(palette = "YlOrRd", domain = countries$CNTR_BN_ID)
 
 ## Read the csv
-#data <- read.csv("pizzamap.csv")
-data<-read.delim("liveseed_data.txt", header = T)
+data<-read.delim("liveseed_data.txt", header = T, fileEncoding = "UTF-8")
+#remove lines with no lat,long
+data<-data[!is.na(data$lat),]
 
 #html hyperlink
 data <- data %>% 
-  mutate(tag = paste0("Web Link: <a href=", link,">", link, "</a>"))
+  mutate(tag = paste0("Web Link: <a href=", link," target=_blank>",  link, "</a>"))
 
-## Create a html popup
+#subset
+dataOPB<-data[which(data$typeOPB=="OPB"),]
+dataDOPB<-data[which(data$typeDOPB=="DOPB"),]
+dataBfO<-data[which(data$typeBfO=="BfO"),]
+dataCB_OS<-data[which(data$typeCB_OS=="CB_OS"),]
+dataother<-data[which(data$type_other=="other"),]
+
+
+
+
+## Create a html popup all
 content <- paste(sep = "<br/>",
                         paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
                         paste0("<b>", data$name),
-                        paste0("<b>", "Breeding type is ", data$type),
                         paste0(data$tag),
                         paste0("</div>"))
 
+## Create a html popup OPB
+contentOPB <- paste(sep = "<br/>",
+                 paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
+                 paste0("<b>", dataOPB$name),
+                 paste0(dataOPB$tag),
+                 paste0("</div>"))
 
+## Create a html popup DOPB
+contentDOPB <- paste(sep = "<br/>",
+                 paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
+                 paste0("<b>", dataDOPB$name),
+                 paste0(dataDOPB$tag),
+                 paste0("</div>"))
+
+## Create a html popup BfO
+contentBfO <- paste(sep = "<br/>",
+                 paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
+                 paste0("<b>", dataBfO$name),
+                 paste0(dataBfO$tag),
+                 paste0("</div>"))
+
+## Create a html popup CB_OS
+contentCB_OS <- paste(sep = "<br/>",
+                 paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
+                 paste0("<b>", dataCB_OS$name),
+                 paste0(dataCB_OS$tag),
+                 paste0("</div>"))
+
+## Createother a html popup other
+contentother <- paste(sep = "<br/>",
+                 paste0("<div class='leaflet-popup-scrolled' style='max-width:200px;max-height:200px'>"),
+                 paste0("<b>", dataother$name),
+                 paste0(dataother$tag),
+                 paste0("</div>"))
 
 ## PART 2 - IN THIS PART THE CODE ADDS ELEMENT ON THE MAP LIKE POLYGONS, POINTS AND IMAGES.
 
 m <- leaflet() %>%
   ## Basemap
-  ##addTiles(tile)        %>%
-  addProviderTiles(providers$CartoDB.Positron)  %>%
+  #addTiles(tile)        %>%
+  addProviderTiles(providers$OpenStreetMap)  %>%
   
   ## Add a zoom reset button
   addResetMapButton() %>%
@@ -58,27 +101,27 @@ m <- leaflet() %>%
   ## Add a coordinates reader
   leafem::addMouseCoordinates() %>%
   ## define the view
-  setView(lng = 24, 
-          lat = 49, 
+  setView(lng = 3, 
+          lat = 60, 
           zoom = 3 ) %>%
   
   ## Add the polygons of the shapefiles
-  addPolygons(data = countries,
-             stroke = FALSE, 
-            smoothFactor = 0.2, 
-              fillOpacity = 1,
-             color = ~palette_countries(NUMBER),
-            group = "Number by Country",
-            label = ~paste("In", NAME, "there are", NUMBER, " initiatives", sep = " ")) %>%
+ # addPolygons(data = countries,
+#             stroke = FALSE, 
+  #          smoothFactor = 0.2, 
+  #              fillOpacity = 1,
+  #           color = ~palette_countries(NUMBER),
+  #          group = "Number by Country",
+  #          label = ~paste("In", NAME, "there are", NUMBER, " initiatives", sep = " ")) %>%
  
    ## Add a legend with the occurrences of the toponyms according to the macro areas
-  addLegend("bottomleft", 
-          pal = palette_countries, 
-           values = countries$NUMBER,
-           title = "N (all types) by country:",
-            labFormat = labelFormat(suffix = " Organisations"),
-            opacity = 1,
-            group = "Number by Country") %>%
+#  addLegend("bottomleft", 
+#         pal = palette_countries, 
+#            values = countries$NUMBER,
+#            title = "N (all types) by country:",
+#             labFormat = labelFormat(suffix = " Organisations"),
+#             opacity = 1,
+#             group = "Number by Country") %>%
   
   ## Add Markers with clustering options
   addAwesomeMarkers(data = data, 
@@ -90,44 +133,46 @@ m <- leaflet() %>%
                     clusterOptions = markerClusterOptions())%>%
   
   ## Add Markers for OPB
-  addAwesomeMarkers(data = data[which(data$typeOPB=="OPB"),], 
+  addAwesomeMarkers(data = dataOPB, 
                     lng = ~long,
                     lat = ~lat, 
-                    popup = c(content), 
+                    popup = c(contentOPB), 
                     group = "Organic Plant Breeding (OPB)",
                     options = popupOptions(maxWidth = 100, maxHeight = 150), 
                     )%>%
   
   ## Add Markers with clustering optionsfor DOPB
-  addAwesomeMarkers(data = data[which(data$typeDOPB=="DOPB"),], 
+  addAwesomeMarkers(data = dataDOPB, 
                     lng = ~long,
                     lat = ~lat, 
-                    popup = c(content), 
+                    popup = c(contentDOPB), 
                     group = "Decentralised Organic Plant Breeding (DOPB)",
                     options = popupOptions(maxWidth = 100, maxHeight = 150), 
                     )%>%
   ## Add Markers for BfO
-  addAwesomeMarkers(data = data[which(data$typeBfO=="BfO"),], 
+  addAwesomeMarkers(data = dataBfO, 
                     lng = ~long,
                     lat = ~lat, 
-                    popup = c(content), 
+                    popup = c(contentBfO), 
                     group = "Breeding for Organic (BfO)",
                     options = popupOptions(maxWidth = 100, maxHeight = 150), 
                    )%>%
   
   ## Add Markers for CB_OS
-  addAwesomeMarkers(data = data[which(data$typeCB_OS=="CB_OS"),], 
-                    lng = ~long,
-                    lat = ~lat, 
-                    popup = c(content), 
-                    group = "Conventiona lbreeding, organic seed production (CBOS)",
-                    options = popupOptions(maxWidth = 100, maxHeight = 150), 
-                   )%>%
+ # addAwesomeMarkers(data = dataCB_OS, 
+ #                   lng = ~long,
+ #                   lat = ~lat, 
+ #                   popup = c(contentCB_OS), 
+ #                   group = "Conventional breeding, organic seed production (CBOS)",
+ #                   options = popupOptions(maxWidth = 100, maxHeight = 150), 
+  #                 )%>%
+  
+  
   ## Add Markers for other
-  addAwesomeMarkers(data = data[which(data$type_other=="other"),], 
+  addAwesomeMarkers(data = dataother, 
                     lng = ~long,
                     lat = ~lat, 
-                    popup = c(content), 
+                    popup = c(contentother), 
                     group = "Type of breeding activity not specified",
                     options = popupOptions(maxWidth = 100, maxHeight = 150), 
   )%>%
@@ -136,7 +181,7 @@ m <- leaflet() %>%
   addLegend("topright", 
             
             colors = c("trasparent"),
-            labels=c("Map based on data by ENGAGEMENT.BIOBREEDING and LIVESEED (H2020 Grant Agreement No 727230)"),
+            labels=c("Plant Breeding Initiatives"),
             
             title="")%>%
   
@@ -149,11 +194,11 @@ m <- leaflet() %>%
                                   "Organic Plant Breeding (OPB)",
                                   "Decentralised Organic Plant Breeding (DOPB)",
                                   "Breeding for Organic (BfO)",
-                                  "Conventional breeding, organic seed production (CBOS)",
+                                  #"Conventional breeding, organic seed production (CBOS)",
                                   "Type of breeding activity not specified",
-                                "Empty layer"),
+                                  "Empty layer"),
                    
-                   overlayGroups = c("Number by Country"),
+                   #overlayGroups = c("Number by Country"),
                    
                    options = layersControlOptions(collapsed = F)) %>%
   
